407e0028f1d9ca82a93931464bedba890ade7851
Shubham <pawar.shubham2597@gmail.com>
2021-07-27 15:39:43 +0530

    new

--- resume/settings.py
+++ resume/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATIC_DIRS = [ 
     os.path.join(BASE_DIR, 'static')
+
 
 
 ]
