af63b914212d61b6de433580a64ea9c715c1b087
Shubham <pawar.shubham2597@gmail.com>
2021-07-28 11:09:25 +0530

    fully

--- resume/settings.py
+++ resume/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATIC_FILES = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_DIRS = [ 
     os.path.join(BASE_DIR, 'static')
 
